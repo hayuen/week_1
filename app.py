b6384a3dba674559fa2c70f8c1f52ae9177e76e5
Hyolim Lim <109011766+hyolimlim@users.noreply.github.com>
2022-07-14 17:31:08 +0900

    완성본
    수고하셨습니다^^

--- app.py
+++ app.py
@@ -202,7 +202,5 @@ def update_like():
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-=======
+    app.run('0.0.0.0', port=3000, debug=True)
